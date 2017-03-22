8b4d9529eb0ed0d48814a2d33e0c380912f1a515
Harry Bloom <harrybloom18@gmail.com>
2017-03-22 12:11:57 +0000

    Initial Commit

---
+++ EmailActionSheet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmailActionSheet
+//
+//  Created by Harry Bloom on 22/03/2017.
+//  Copyright © 2017 WeVat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
